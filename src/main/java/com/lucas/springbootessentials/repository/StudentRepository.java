e5ba69f379a31872602a8a3609642de9b734e924
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2020-09-24 11:16:56 -0300

    Repository Student

--- src/main/java/com/lucas/springbootessentials/repository/StudentRepository.java
+++ src/main/java/com/lucas/springbootessentials/repository/StudentRepository.java
@@ -1,10 +1,14 @@
 package com.lucas.springbootessentials.repository;
 
 import com.lucas.springbootessentials.model.Student;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+	List<Student> findByName(String name);
 }

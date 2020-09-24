1b42698725530940df3f1906f8684c75425d5f46
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2020-09-24 11:18:40 -0300

    Exception ResourceNotfFound

---
+++ src/main/java/com/lucas/springbootessentials/exception/ResourceNotFoundDetails.java
@@ -0,0 +1,62 @@
+package com.lucas.springbootessentials.exception;
+
+public class ResourceNotFoundDetails {
+	
+	private String title;
+	private int status;
+	private String detail;
+	private long timestamp;
+	private String developerMessage;
+
+	public ResourceNotFoundDetails(String title, int status, String detail, long timestamp, String developerMessage) {
+		super();
+		this.title = title;
+		this.status = status;
+		this.detail = detail;
+		this.timestamp = timestamp;
+		this.developerMessage = developerMessage;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getDeveloperMessage() {
+		return developerMessage;
+	}
+
+	public void setDeveloperMessage(String developerMessage) {
+		this.developerMessage = developerMessage;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/lucas/springbootessentials/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.lucas.springbootessentials.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@SuppressWarnings("serial")
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}

b4c072dc250120880677962ad48fabdc8d31697d
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2020-09-24 11:17:20 -0300

    Model Student e ID

--- src/main/java/com/lucas/springbootessentials/model/Student.java
+++ src/main/java/com/lucas/springbootessentials/model/Student.java
@@ -1,47 +1,20 @@
 package com.lucas.springbootessentials.model;
 
 import javax.persistence.Entity;
-import java.util.Objects;
 
+@SuppressWarnings("serial")
 @Entity
 public class Student extends ID {
 
-    private String nome;
+    private String name;
 
-    private String email;
+	public String getName() {
+		return name;
+	}
 
-    public Student(Long id,String nome) {
-        this.id = id;
-        this.nome = nome;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Student student = (Student) o;
-        return Objects.equals(nome, student.nome);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), nome);
-    }
+    
 }

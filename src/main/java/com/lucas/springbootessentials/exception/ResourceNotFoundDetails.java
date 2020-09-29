67af8ffc638b8cf7df007b25fdb4d9f560c4880f
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2020-09-28 22:28:54 -0300

    Updating resource exception

--- src/main/java/com/lucas/springbootessentials/exception/ResourceNotFoundDetails.java
+++ src/main/java/com/lucas/springbootessentials/exception/ResourceNotFoundDetails.java
@@ -1,62 +1,13 @@
 package com.lucas.springbootessentials.exception;
 
-public class ResourceNotFoundDetails {
-	
-	private String title;
-	private int status;
-	private String detail;
-	private long timestamp;
-	private String developerMessage;
+public class ResourceNotFoundDetails extends ErrorDetail{
 
 	public ResourceNotFoundDetails(String title, int status, String detail, long timestamp, String developerMessage) {
 		super();
-		this.title = title;
-		this.status = status;
-		this.detail = detail;
-		this.timestamp = timestamp;
-		this.developerMessage = developerMessage;
+		setTitle(title);
+		setStatus(status);
+		setDetail(detail);
+		setTimestamp(timestamp);
+		setDeveloperMessage(developerMessage);
 	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
-
-	public String getDetail() {
-		return detail;
-	}
-
-	public void setDetail(String detail) {
-		this.detail = detail;
-	}
-
-	public long getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(long timestamp) {
-		this.timestamp = timestamp;
-	}
-
-	public String getDeveloperMessage() {
-		return developerMessage;
-	}
-
-	public void setDeveloperMessage(String developerMessage) {
-		this.developerMessage = developerMessage;
-	}
-	
-	
-
 }

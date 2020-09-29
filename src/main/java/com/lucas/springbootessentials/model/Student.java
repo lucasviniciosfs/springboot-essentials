3da4856a6895927e77c29fd02c78e084e923f9ce
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2020-09-28 22:27:32 -0300

    Validation Entity Student

--- src/main/java/com/lucas/springbootessentials/model/Student.java
+++ src/main/java/com/lucas/springbootessentials/model/Student.java
@@ -1,12 +1,22 @@
 package com.lucas.springbootessentials.model;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @SuppressWarnings("serial")
 @Entity
 public class Student extends ID {
 
+	@NotEmpty(message = "Nome do aluno é obrigatório!")
+	@NotNull(message = "Nome do aluno é obrigatório!")
     private String name;
+
+	@NotEmpty(message = "Email do aluno é obrigatório!")
+	@NotNull(message = "Email do aluno é obrigatório!")
+	@Email
+	private String email;
 
 	public String getName() {
 		return name;
@@ -16,5 +26,11 @@ public class Student extends ID {
 		this.name = name;
 	}
 
-    
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
